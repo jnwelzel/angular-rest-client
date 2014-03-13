9bdc06a2e8281cf87f329efefa692b8413b21435
Jon Welzel <jonathan.welzel@totvs.com.br>
2014-03-13 18:10:46 -0300

    Mostrando todos users cadastrados ja

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,7 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'UsersController'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/users'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', [function() {
 
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('UsersController', ['$scope', '$http', function($scope, $http) {
+    $http.get('http://10.80.81.67:8080/rest/resources/users').
+      success(function(data) {
+        $scope.users = data;
+      });
   }]);
